d5466e21a90e2c25e3b1023f821c1625d4732e5d
snuzj <daovinh1342002@gmail.com>
2023-12-20 17:46:04 +0700

    addRequestCamera

--- webcam/scripts.js
+++ webcam/scripts.js
@@ -1,88 +1,58 @@
-// Create a Three.js scene
 const scene = new THREE.Scene();
-
-// Create a Three.js camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Create a Three.js WebGLRenderer
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+const camera = new THREE.Camera();
+scene.add(camera)
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha:true
+});
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a video element for the webcam feed
-const video = document.createElement('video');
+var ArToolkitSource = new THREEx.ArToolkitSource({
+    sourceType: "webcam",
+})
 
-// Wait for the webcam to initialize
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function (stream) {
-        // Attach the webcam stream to the video element
-        video.srcObject = stream;
-        video.play();
+ArToolkitSource.init(function(){
+    setTimeout(function (){
+        ArToolkitSource.onResizeElement();
+        ArToolkitSource.copyElementSizeTo(renderer.domElement);
+    },2000)
+})
 
-        // Wait for video metadata to ensure correct sizing
-        video.onloadedmetadata = function () {
-            // Initialize ARToolkitSource with the webcam feed
-            const ArToolkitSource = new THREEx.ArToolkitSource({
-                sourceType: 'webcam',
-                source: video
-            });
+var ArToolkitContext = new THREEx.ArToolkitContext({
+    cameraParametersUrl: ' camera_para.dat',
+    detectionMode: 'color_and_matrix',
+})
 
-            // Initialize ARToolkitSource and adjust the renderer size after a delay
-            ArToolkitSource.init(function () {
-                setTimeout(function () {
-                    ArToolkitSource.onResizeElement();
-                    ArToolkitSource.copyElementSizeTo(renderer.domElement);
-                }, 2000);
-            });
+ArToolkitContext.init(function () {
+    camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
+})
 
-            // Initialize ARToolkitContext with camera parameters
-            const ArToolkitContext = new THREEx.ArToolkitContext({
-                cameraParametersUrl: 'camera_para.dat',
-                detectionMode: 'color_and_matrix'
-            });
+var ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext,camera,{
+    type: "pattern",
+    patternUrl: "assets/pattern_bard.patt",
+    changeMatrixMode: "cameraTransformMatrix"
+});
 
-            // Initialize ARToolkitContext and set camera projection matrix
-            ArToolkitContext.init(function () {
-                camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
-            });
+scene.visible = false;
 
-            // Create ARMarkerControls for pattern detection
-            const ArMarkerControls = new THREEx.ArMarkerControls(
-                ArToolkitContext,
-                camera,
-                {
-                    type: 'pattern',
-                    patternUrl: 'assets/pattern_bard.patt',
-                    changeMatrixMode: 'cameraTransformMatrix'
-                }
-            );
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshNormalMaterial({
+    transparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide
+});
 
-            // Create a simple cube for visualization
-            const geometry = new THREE.BoxGeometry(1, 1, 1);
-            const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-            const cube = new THREE.Mesh(geometry, material);
-            cube.position.y = geometry.parameters.height / 2;
-            scene.add(cube);
+const cube = new THREE.Mesh(geometry, material);
+cube.position.y = geometry.parameters.height / 2;
+scene.add(cube);
 
-            // Set the initial camera position
-            camera.position.z = 5;
+function animate() {
+    requestAnimationFrame( animate );
+    ArToolkitContext.update(ArToolkitSource.domElement);
+    scene.visible = camera.visible;
+    renderer.render( scene, camera );
+}
 
-            // Animation loop
-            function animate() {
-                requestAnimationFrame(animate);
+animate();
 
-                // Update ARToolkitContext and set scene visibility
-                ArToolkitContext.update(ArToolkitSource.domElement);
-                scene.visible = camera.visible;
-
-                // Render the scene
-                renderer.render(scene, camera);
-            }
-
-            // Start the animation loop
-            animate();
-        };
-    })
-    .catch(function (error) {
-        console.error('Error accessing webcam:', error);
-    });
